ca7e44acc618360d1f1df90e2a142750e7fc6a0b
Артем <artemkorolev-2002@yandex.ru>
2023-11-21 00:38:14 +0300

    Refactored code

--- Main.py
+++ Main.py
@@ -9,16 +9,22 @@ def getSampleWithUniformDistribuition(left_bound, length, size):
 
 
 def getSampleWithNormalDistribution(size, mean, var):
-    return np.random.randn(size) * var + mean
+    return np.random.normal(mean, var, size=size)
 
 
 def getStandartDeviationFromVariance(sample):
     return sample.var(ddof=1)
 
 
-def Mann_whitneyu(sample1, sample2, alternative='two-sided'):
-    statistic, _ = sps.mannwhitneyu(sample1, sample2, alternative=alternative)
-    return statistic, _
+def Mann_whitneyu(sample1, sample2, alternative='two-sided', alpha=0.05):
+    statistic, p_value = sps.mannwhitneyu(sample1, sample2, alternative=alternative)
+    has_difference = p_value < alpha
+    if has_difference:
+        print("There is statistical differences between samples")
+    else:
+        print("There is no statistical differences between samples")
+
+    return has_difference
 
 
 def calculate_confidence_interval(data, alpha=0.05):
@@ -26,16 +32,28 @@ def calculate_confidence_interval(data, alpha=0.05):
     return lower, upper
 
 
-def tTest(sample1, sample2, alternative='two-sided'):
+def tTest(sample1, sample2, alternative='two-sided', alpha=0.05):
     t_statistic, p_value = sps.ttest_ind(sample1, sample2, alternative=alternative)
-    return t_statistic, p_value
+    has_difference = p_value < alpha
+    if has_difference:
+        print("There is statistical differences between samples")
+    else:
+        print("There is no statistical differences between samples")
+
+    return has_difference
 
 
-def z_test_proportions(successes1, trials1, successes2, trials2, alternative='two-sided', value=0):
+def z_test_proportions(successes1, trials1, successes2, trials2, alternative='two-sided', value=0,alpha=0.05):
     z_stat, p_value = sm.stats.proportions_ztest(
         [successes1, successes2],
         [trials1, trials2],
         alternative=alternative,
         value=value
     )
-    return z_stat, p_value
+    has_difference = p_value < alpha
+    if has_difference:
+        print("There is statistical differences between samples")
+    else:
+        print("There is no statistical differences between samples")
+
+    return has_difference
--- Tests.py
+++ Tests.py
@@ -8,13 +8,17 @@ class Tests(unittest.TestCase):
     def testMann_Withney_true(self):
         sample1 = getSampleWithUniformDistribuition(1, 5, 200)
         sample2 = getSampleWithUniformDistribuition(1, 5, 100)
-        self.assertEquals(Mann_whitneyu(sample1, sample2), sps.mannwhitneyu(sample1, sample2))
+        self.assertEqual(Mann_whitneyu(sample1, sample2), False)
 
     def test_mann_withney_statictik(self):
         sample1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         sample2 = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-        self.assertEqual(Mann_whitneyu(sample1, sample2)[0], 12.5)
+        self.assertTrue(Mann_whitneyu(sample1, sample2))
 
     def test_z_Proportion(self):
         rez = z_test_proportions(165, 300, 196, 400)
-        self.assertTrue(rez[0] - 1.57195 < 0)
+        self.assertFalse(rez)
+
+    def test_z_Proportion2(self):
+        rez = z_test_proportions(100,200,10,500)
+        self.assertTrue(rez)

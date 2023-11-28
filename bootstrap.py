d0906c32659efef804ad23846f22a2240010bd1a
emazhara <mazharaevgeniy@gmail.com>
2023-11-28 04:28:08 +0000

    update bootstrap using mean instead of quantile

--- bootstrap.py
+++ bootstrap.py
@@ -1,35 +1,44 @@
 import numpy as np
 from tqdm import tqdm
 import scipy.stats as stas
+from matplotlib import pyplot as plt
+import math
 
 def bootstrap(values_ctrl, values_var, iter=1000, alpha=0.05):
 
     size_ctrl = len(values_ctrl)
     size_var = len(values_var)
+    size = size_var + size_ctrl
 
-    ctrl_quantiles = []
-    var_quantiles = []
+    diff = round(np.mean(values_var) - np.mean(values_ctrl), 2)
+    diffs = []
 
     for _ in tqdm(range(iter)):
         sample_ctrl = np.random.choice(a=values_ctrl, size=size_ctrl, replace=True)
         sample_var = np.random.choice(a=values_var, size=size_var, replace=True)
+        diffs.append(np.mean(sample_var) - np.mean(sample_ctrl))
 
-        ctrl_quantiles.append(np.quantile(sample_ctrl, 1 - alpha))
-        var_quantiles.append(np.quantile(sample_var, 1 - alpha))
+    plot_diffs = np.round(diffs, 2)
+    unique, counts = np.unique(plot_diffs, return_counts=True)
 
-    std_ctrl = np.std(ctrl_quantiles)
-    std_var = np.std(var_quantiles)
+    plt.bar(unique, counts)
+    plt.show()
 
-    point_est = np.mean(ctrl_quantiles) - np.mean(var_quantiles)
+    p_value = 1 - len(list(filter(lambda x: abs(x) > 0, diffs))) / iter
+    print('p-value:', p_value)
+
+    point_est = diff
+    std_diff = np.std(diffs)
     z_crit = stas.norm.ppf(1 - alpha / 2)
-    std = (std_ctrl ** 2 / size_ctrl + std_var ** 2 / size_var) ** 0.5
-    conf_int = (point_est - z_crit * std, point_est + z_crit * std)
+    conf_int = (round(point_est - z_crit * std_diff / math.sqrt(size), 2), round(point_est + z_crit * std_diff / math.sqrt(size), 2))
+    print('confidence interval with alpha=', alpha, ':', conf_int)
 
-    has_stat_diff = not (conf_int[0] <= 0 <= conf_int[1])
+    #has_stat_diff = not (conf_int[0] <= 0 <= conf_int[1])
+    has_stat_diff = p_value <= alpha
 
     if (has_stat_diff):
         print('There are statistically significant changes in the control group')
     else:
         print('There are NO statistically significant changes in the control group')
 
-    return has_stat_diff
+    return has_stat_diff, p_value, conf_int
--- bootstrap_test.py
+++ bootstrap_test.py
@@ -8,13 +8,13 @@ class TestBootstrap(unittest.TestCase):
     values_b1 = [3, 3, 3, 3, 3]
 
     values_a2 = [5, 5, 5, 5, 5]
-    values_b2 = [5, 5, 5, 5, 4.99]
+    values_b2 = [5, 5, 5, 4.9, 4.9]
 
     def setUp(self):
         self.bs = bootstrap
 
     def test_bs_true(self):
-        self.assertEqual(self.bs(self.values_a1, self.values_b1), True)
+        self.assertEqual(self.bs(self.values_a1, self.values_b1)[0], True)
 
     def test_bs_false(self):
-        self.assertEqual(self.bs(self.values_a2, self.values_b2), False)
+        self.assertEqual(self.bs(self.values_a2, self.values_b2)[0], False)

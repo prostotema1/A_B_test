a0622238cfcbd2ab9b8b1ba079bf9f84fd983113
Артем <artemkorolev-2002@yandex.ru>
2023-12-05 09:55:35 +0300

    Rename methods

--- Main.py
+++ Main.py
@@ -4,25 +4,25 @@ import statsmodels.api as sm
 import statsmodels.stats.proportion as prop
 
 
-def getSampleWithUniformDistribuition(left_bound, length, size):
+def get_sample_with_uniform_distribuition(left_bound, length, size):
     return sps.uniform(loc=left_bound, scale=length).rvs(size)
 
 
-def getSampleWithNormalDistribution(size, mean, var):
+def get_sample_with_normal_distribution(size, mean, var):
     return np.random.normal(mean, var, size=size)
 
 
-def getStandartDeviationFromVariance(sample):
+def get_standart_deviation_from_variance(sample):
     return sample.var(ddof=1)
 
 
-def Mann_whitneyu(sample1, sample2, alternative='two-sided', alpha=0.05):
+def mann_whitneyu(sample1, sample2, alternative='two-sided', alpha=0.05):
     statistic, p_value = sps.mannwhitneyu(sample1, sample2, alternative=alternative)
     has_difference = p_value < alpha
-    if has_difference:
-        print("There is statistical differences between samples")
-    else:
-        print("There is no statistical differences between samples")
+    # if has_difference:
+    #     print("There is statistical differences between samples")
+    # else:
+    #     print("There is no statistical differences between samples")
 
     return p_value
 
@@ -32,27 +32,27 @@ def calculate_confidence_interval(data, alpha=0.05):
     return lower, upper
 
 
-def tTest(sample1, sample2, alternative='two-sided', alpha=0.05):
+def t_test(sample1, sample2, alternative='two-sided', alpha=0.05):
     t_statistic, p_value = sps.ttest_ind(sample1, sample2, alternative=alternative)
     has_difference = p_value < alpha
-    if has_difference:
-        print("There is statistical differences between samples")
-    else:
-        print("There is no statistical differences between samples")
+    # if has_difference:
+    #     print("There is statistical differences between samples")
+    # else:
+    #     print("There is no statistical differences between samples")
 
     return p_value
 
 
-def compare_tTest_and_Mann_Whitney(counter=1000) -> bool:
+def compare_t_test_and_mann_whitney(counter=1000) -> bool:
     counter_t_test = 0
     counter_manna_whitney = 0
     k = counter
     while k >= 0:
         k -= 1
-        test = getSampleWithUniformDistribuition(-1,2,1000)
-        control = getSampleWithUniformDistribuition(-100,200,1000)
-        t_Test_result = tTest(test, control)
-        manna_whit_res = Mann_whitneyu(test,control)
+        test = get_sample_with_uniform_distribuition(-1, 2, 1000)
+        control = get_sample_with_uniform_distribuition(-100, 200, 1000)
+        t_Test_result = t_test(test, control)
+        manna_whit_res = mann_whitneyu(test, control)
 
         if t_Test_result < 0.05:
             counter_t_test += 1
--- Tests.py
+++ Tests.py
@@ -7,18 +7,18 @@ from Main import *
 class Tests(unittest.TestCase):
 
     def test_t_Test(self):
-        sample1 = getSampleWithUniformDistribuition(-1,2,1000)
-        sample2 = getSampleWithUniformDistribuition(-100,200,1000)
-        self.assertFalse(tTest(sample1,sample2) < 0.05)
+        sample1 = get_sample_with_uniform_distribuition(-1, 2, 1000)
+        sample2 = get_sample_with_uniform_distribuition(-100, 200, 1000)
+        self.assertFalse(t_test(sample1, sample2) < 0.05)
     def testMann_Withney_true(self):
-        sample1 = getSampleWithUniformDistribuition(1, 5, 200)
-        sample2 = getSampleWithUniformDistribuition(1, 5, 100)
-        self.assertFalse(Mann_whitneyu(sample1, sample2) < 0.05)
+        sample1 = get_sample_with_uniform_distribuition(1, 5, 200)
+        sample2 = get_sample_with_uniform_distribuition(1, 5, 100)
+        self.assertFalse(mann_whitneyu(sample1, sample2) < 0.05)
 
     def test_mann_withney_statictik(self):
         sample1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         sample2 = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-        self.assertTrue(Mann_whitneyu(sample1, sample2) < 0.05)
+        self.assertTrue(mann_whitneyu(sample1, sample2) < 0.05)
 
     def test_z_Proportion(self):
         rez = z_test_proportions(165, 300, 196, 400)
@@ -29,5 +29,5 @@ class Tests(unittest.TestCase):
         self.assertTrue(rez)
 
     def test_t_Test_vs_manna_whitney(self):
-        rez = compare_tTest_and_Mann_Whitney(1000)
+        rez = compare_t_test_and_mann_whitney(10000)
         self.assertTrue(rez)
